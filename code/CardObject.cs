706007b33bc40020e62fbd773c2142c3754ddb4f
rainthstrive <rainthstrive@gmail.com>
2024-03-14 05:04:44 -0600

    todo: player actions, and show them

--- code/CardObject.cs
+++ code/CardObject.cs
@@ -4,19 +4,21 @@ public sealed class CardObject : Component
 {
     public string Name { get; set; }
 	public int EnergyCost { get; set; }
+    public string Type { get; set; }
     public string Description { get; set; }
 
 	// Constructor
-   public void SetProperties(string name, int energyCost, string description)
+   public void SetProperties(string name, int energyCost, string type, string description)
     {
         Name = name;
         EnergyCost = energyCost;
+        Type = type;
         Description = description;
     }
 
     public void ReadProperties()
     {
-        Log.Info("Card values: " + Name + "/" + EnergyCost + "/" + Description);
+        Log.Info("Card values: " + Name + "/" + EnergyCost + "/"  + Type + "/" + Description);
     }
 
     // Example Effect Method
---
+++ code/PlayerProperties.cs
@@ -0,0 +1,41 @@
+using System.Text.Json;
+using System.IO;
+class PlayerProperties
+{
+    public void SaveDeckToFile(Deck deck, string filePath)
+    {
+        var options = new JsonSerializerOptions { WriteIndented = true };
+        string jsonString = JsonSerializer.Serialize(deck, options);
+        File.WriteAllText(filePath, jsonString);
+    }
+
+    // todo: replace void for Deck type
+    public void LoadDeckFromFile(string filePath)
+    {
+        string jsonString = File.ReadAllText(filePath);
+        Deck deck = JsonSerializer.Deserialize<Deck>(jsonString);
+        //return deck;
+    }
+}
+
+/*
+string deckFilePath = "path/to/your/deckfile.json";
+
+if (!File.Exists(deckFilePath))
+{
+    Deck initialDeck = new Deck();
+    // Add initial cards to the deck
+    initialDeck.AddCard(new Card("Sword Slash", 1, "Attack", "Deal 10 damage."));
+    initialDeck.AddCard(new Card("Shield Up", 1, "Defense", "Gain 10 block."));
+    // Add more cards as needed
+    SaveDeckToFile(initialDeck, deckFilePath);
+}
+______________
+
+Deck deck = LoadDeckFromFile(deckFilePath);
+// Example: Add a new card
+deck.AddCard(new Card("Fireball", 2, "Attack", "Deal 15 damage to all enemies."));
+// Save changes
+SaveDeckToFile(deck, deckFilePath);
+
+*/
