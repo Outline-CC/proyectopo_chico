71daaa47c422649d6625a6eec7cefaffb2a6c91d
Empire\Robs <roberto.esparza90@gmail.com>
2024-03-17 02:38:09 -0600

    Expand enemy's properties and EnemyAction

--- code/Enemy.cs
+++ code/Enemy.cs
@@ -1,23 +1,60 @@
 using Sandbox;
 
+public enum EnemyActionType
+{
+	NormalAttack,
+	Buff,
+	Debuff,
+	SpecialAttack
+}
+
 public sealed class Enemy : Component
 {
-
+	public EnemyActionType enemyAction {  get;  set; } = EnemyActionType.NormalAttack;
 	public int LifePoints {get; set;}
 	public int Strength {get; set;}
 	public int Defense {get; set;}
 	public int Energy {get; set;}
-	public Deck MyDeck {get; set;}
+	public float Accuracy { get; set;}
 
 	public Enemy()
 	{
 		LifePoints = 20;
 		Energy = 3;
-		MyDeck = new Deck();
+		Strength = 0; 
+		Defense = 0;
+		Accuracy = 0;
 	}
 
 	protected override void OnUpdate()
 	{
 
+	}
+
+	public void Action() 
+	{
+		switch (enemyAction)
+		{
+			case EnemyActionType.NormalAttack:
+				//Logica de Ataque Normal
+				break;
+			case EnemyActionType.Buff:
+				//Logica de Buff
+				break;
+			case EnemyActionType.Debuff:
+				//Logica de Debuff
+				break;
+			case EnemyActionType.SpecialAttack:
+				//Logica de Ataque Especial
+				break;
+			default:
+				break;
+		}
+
+		enemyAction++;
+		if(enemyAction > EnemyActionType.SpecialAttack)
+		{
+			enemyAction = EnemyActionType.NormalAttack;
+		}
 	}
 }
---
+++ code/EnemyAction.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Sandbox
+{
+	internal class EnemyAction
+	{
+		private string _name;
+		private int _energyCost;
+		private int _damage;
+		private int _opponentDefense;
+		private int _opponentAttack;
+		private int _myDefense;
+		private int _myAttack;
+		private string _description;
+		[Property]
+		public string Name 
+		{ 
+			get { return _name; } 
+			set { _name = value; } 
+		}
+		[Property]
+		public int EnergyCost
+		{
+			get { return _energyCost; }
+			set { _energyCost = value; }
+		}
+		[Property]
+		public int Damage
+		{
+			get { return _damage; }
+			set { _damage = value; }
+		}
+		[Property]
+		public int OpponentDefense
+		{
+			get { return _opponentDefense; }
+			set { _opponentDefense = value; }
+		}
+		[Property]
+		public int OpponentAttack
+		{
+			get { return _opponentAttack; }
+			set { _opponentAttack = value; }
+		}
+		[Property]
+		public int MyDefense
+		{
+			get { return _myDefense; }
+			set { _opponentDefense = value; }
+		}
+		[Property]
+		public int MyAttack
+		{
+			get { return _myAttack; }
+			set { _opponentAttack = value; }
+		}
+		[Property]
+		public string Description
+		{
+			get { return _description; }
+			set { _description = value; }
+		}
+		public EnemyAction(string name, int energyCost, int damage, int opponentDefense, int opponentAttack, int myDefense, int myAttack, string Description) 
+		{
+			_name = name;
+			_energyCost = energyCost;
+			_damage = damage;
+			_opponentDefense = opponentDefense;
+			_opponentAttack = opponentAttack;
+			_myDefense = myDefense;
+			_myAttack = myAttack;
+			_description = Description;
+		}
+
+		public void ResolveAction(ref Player player, ref Enemy enemy)
+		{
+			if ( player != null)
+			{
+				player.LifePoints -= _damage;
+				player.Strength -= _opponentAttack;
+				player.Defense -= _opponentDefense;
+			}
+			if (enemy != null)
+			{
+				enemy.Energy -= _energyCost;
+				enemy.Strength += _myAttack;
+				enemy.Defense += _myDefense;
+			}
+		}
+	}
+}
